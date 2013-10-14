9672c4d14896f7233276e59bd66b77979538bcf5
xboston <bost56@gmail.com>
2013-10-15 00:03:59 +0600

    Ignore Volt cache

--- app/cache/_home_boston_www_phalcon-auth_app_views_users_index.volt.php
+++
@@ -1,9 +0,0 @@
-<h2>User Auth!</h2>
-
-<div class="ui horizontal icon divider">
-    <i class="circular heart icon"></i>
-</div>
-
-<?php echo $this->getContent(); ?>
-
-Hello <?php echo ($authUser ? $authUser->name : 'Guest'); ?>
